66043d6195bb33935e01175b21e7c4aa57398bfa
aamirbig <alireza_amirbiglou@yahoo.com>
2024-09-05 20:40:35 -0400

    Update streamlit_app.py
    added new connection to display fruityvice nutrition info

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
+#new connection to display fruityvice nutrition info
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
